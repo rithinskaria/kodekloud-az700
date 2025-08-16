7bab8d091616ecdf04f4ee2490dad76e005390f9
Rithin Skaria <rithin@rithin.net>
2025-08-16 20:37:13 +0300

    Update lab-private-dns.ps1

--- lab-private-dns.ps1
+++ lab-private-dns.ps1
@@ -24,45 +24,63 @@ $addressPrefix2 = "10.20.0.0/16"
 $subnetPrefix2 = "10.20.1.0/24"
 
 # Create Resource Group
-az group create --name $resourceGroup --location $locationEUS
+New-AzResourceGroup -Name $resourceGroup -Location $locationEUS
 
-# Create VNETs and Subnets
-az network vnet create --resource-group $resourceGroup --name $vnetName1 --address-prefix $addressPrefix1 --location $locationEUS --subnet-name $subnetName1 --subnet-prefix $subnetPrefix1
-az network vnet create --resource-group $resourceGroup --name $vnetName2 --address-prefix $addressPrefix2 --location $locationWUS --subnet-name $subnetName2 --subnet-prefix $subnetPrefix2
+# Create Subnets
+$subnet1 = New-AzVirtualNetworkSubnetConfig -Name $subnetName1 -AddressPrefix $subnetPrefix1
+$subnet2 = New-AzVirtualNetworkSubnetConfig -Name $subnetName2 -AddressPrefix $subnetPrefix2
+
+# Create VNETs
+New-AzVirtualNetwork -Name $vnetName1 -ResourceGroupName $resourceGroup -Location $locationEUS -AddressPrefix $addressPrefix1 -Subnet $subnet1
+New-AzVirtualNetwork -Name $vnetName2 -ResourceGroupName $resourceGroup -Location $locationWUS -AddressPrefix $addressPrefix2 -Subnet $subnet2
 
 # Create Public IPs
-az network public-ip create --resource-group $resourceGroup --name $ipName1 --location $locationEUS
-az network public-ip create --resource-group $resourceGroup --name $ipName2 --location $locationWUS
+New-AzPublicIpAddress -Name $ipName1 -ResourceGroupName $resourceGroup -Location $locationEUS -AllocationMethod Static
+New-AzPublicIpAddress -Name $ipName2 -ResourceGroupName $resourceGroup -Location $locationWUS -AllocationMethod Static
+
+# Get VNETs and Public IPs for NIC creation
+$vnet1 = Get-AzVirtualNetwork -Name $vnetName1 -ResourceGroupName $resourceGroup
+$vnet2 = Get-AzVirtualNetwork -Name $vnetName2 -ResourceGroupName $resourceGroup
+$pip1 = Get-AzPublicIpAddress -Name $ipName1 -ResourceGroupName $resourceGroup
+$pip2 = Get-AzPublicIpAddress -Name $ipName2 -ResourceGroupName $resourceGroup
 
 # Create NICs
-az network nic create --resource-group $resourceGroup --name $nicName1 --vnet-name $vnetName1 --subnet $subnetName1 --public-ip-address $ipName1 --location $locationEUS
-az network nic create --resource-group $resourceGroup --name $nicName2 --vnet-name $vnetName2 --subnet $subnetName2 --public-ip-address $ipName2 --location $locationWUS
+New-AzNetworkInterface -Name $nicName1 -ResourceGroupName $resourceGroup -Location $locationEUS -SubnetId $vnet1.Subnets[0].Id -PublicIpAddressId $pip1.Id
+New-AzNetworkInterface -Name $nicName2 -ResourceGroupName $resourceGroup -Location $locationWUS -SubnetId $vnet2.Subnets[0].Id -PublicIpAddressId $pip2.Id
 
 
 # Create NSGs
 $nsgEUS = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $locationEUS -Name "nsg-az700-eus"
 $nsgWUS = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $locationWUS -Name "nsg-az700-wus"
 
-# Allow SSH on both NSGs
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name "Allow-SSH" -Description "Allow SSH" -Access Allow -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
-$nsgEUS | Add-AzNetworkSecurityRuleConfig -NetworkSecurityRule $nsgRuleSSH | Set-AzNetworkSecurityGroup
-$nsgWUS | Add-AzNetworkSecurityRuleConfig -NetworkSecurityRule $nsgRuleSSH | Set-AzNetworkSecurityGroup
+# Allow SSH on both NSGs 
+$nsgEUS = Get-AzNetworkSecurityGroup -Name "nsg-az700-eus" -ResourceGroupName $resourceGroup
+$nsgEUS = Add-AzNetworkSecurityRuleConfig -Name "Allow-SSH" -Description "Allow SSH" -Access Allow -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -NetworkSecurityGroup $nsgEUS
+Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsgEUS
+
+$nsgWUS = Get-AzNetworkSecurityGroup -Name "nsg-az700-wus" -ResourceGroupName $resourceGroup
+$nsgWUS = Add-AzNetworkSecurityRuleConfig -Name "Allow-SSH" -Description "Allow SSH" -Access Allow -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -NetworkSecurityGroup $nsgWUS
+Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsgWUS
 
 # Attach NSGs to NICs
-Set-AzNetworkInterface -Name $nicName1 -ResourceGroupName $resourceGroup -NetworkSecurityGroup $nsgEUS
-Set-AzNetworkInterface -Name $nicName2 -ResourceGroupName $resourceGroup -NetworkSecurityGroup $nsgWUS
+$nic1 = Get-AzNetworkInterface -Name $nicName1 -ResourceGroupName $resourceGroup
+$nic2 = Get-AzNetworkInterface -Name $nicName2 -ResourceGroupName $resourceGroup
+$nic1.NetworkSecurityGroup = $nsgEUS
+$nic2.NetworkSecurityGroup = $nsgWUS
+Set-AzNetworkInterface -NetworkInterface $nic1
+Set-AzNetworkInterface -NetworkInterface $nic2
 
 # VM config for EUS
 $vmConfig1 = New-AzVMConfig -VMName $vmName1 -VMSize "Standard_B1s" | `
-	Set-AzVMOperatingSystem -Linux -ComputerName $vmName1 -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))) | `
-	Set-AzVMSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "22_04-lts-gen2" -Version "latest" | `
-	Add-AzVMNetworkInterface -Id (Get-AzNetworkInterface -Name $nicName1 -ResourceGroupName $resourceGroup).Id
+    Set-AzVMOperatingSystem -Linux -ComputerName $vmName1 -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))) | `
+    Set-AzVMSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
+    Add-AzVMNetworkInterface -Id (Get-AzNetworkInterface -Name $nicName1 -ResourceGroupName $resourceGroup).Id
 
 # VM config for WUS
 $vmConfig2 = New-AzVMConfig -VMName $vmName2 -VMSize "Standard_B1s" | `
-	Set-AzVMOperatingSystem -Linux -ComputerName $vmName2 -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))) | `
-	Set-AzVMSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "22_04-lts-gen2" -Version "latest" | `
-	Add-AzVMNetworkInterface -Id (Get-AzNetworkInterface -Name $nicName2 -ResourceGroupName $resourceGroup).Id
+    Set-AzVMOperatingSystem -Linux -ComputerName $vmName2 -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))) | `
+    Set-AzVMSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
+    Add-AzVMNetworkInterface -Id (Get-AzNetworkInterface -Name $nicName2 -ResourceGroupName $resourceGroup).Id
 
 # Create VMs
 New-AzVM -ResourceGroupName $resourceGroup -Location $locationEUS -VM $vmConfig1
